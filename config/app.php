f77c9025be052dfc602d086ec770b0170139f53d
Fernando Cosme <cosme.92@gmail.com>
2018-06-13 21:54:50 -0600

    actualizando cabeceras

--- config/app.php
+++ config/app.php
@@ -1,7 +1,5 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-header("Allow: GET, POST, OPTIONS, PUT, DELETE");
 return [
 
     /*
