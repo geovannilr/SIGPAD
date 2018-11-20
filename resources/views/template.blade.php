08091508fa6d3e64e085f5ad4fe929d7f5418f8b
Fernando Cosme <cosme.92@gmail.com>
2018-11-20 00:19:15 -0600

    cambio permiso para docente.

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -62,7 +62,7 @@
               <span class="nav-link-text" style="color: #ffffff" >Trabajo de Graduacion</span>
             </a>
             <ul class="sidenav-second-level collapse" id="collapseTrabajoGraduacion">
-              @can('grupotdg.create')
+              @can('grupotdg.create,grupo.index')
                 <li>
                   <a class="nav-link" href="{{route('grupo.create')}}" style="color: #ffffff">
                     <i class="fa fa-users"></i>
@@ -75,7 +75,7 @@
                   </a>  
               </li>
               @endcan
-              @can('grupo.index')
+              @can('grupoAsesor.index')
                 <li>
                   <a class="nav-link" href="{{route('listadoGrupos')}}" style="color: #ffffff">
                     <i class="fa fa-users"></i>
