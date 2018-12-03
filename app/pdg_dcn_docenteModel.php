659aed7bd08dbfa29615ca4f89843d15c6262461
baltera <baltera@users.noreply.github.com>
2018-12-02 22:26:25 -0600

    Display name en resultado del query

--- app/pdg_dcn_docenteModel.php
+++ app/pdg_dcn_docenteModel.php
@@ -258,6 +258,7 @@ class pdg_dcn_docenteModel extends Model
             COALESCE(dcn.dcn_profileFoto ,'https://profile.actionsprout.com/default.jpeg') as dcn_profileFoto,
             dcn.tipoJornada,
             dcn.perfilPrivado
+            ,dcn.display_name
             from pdg_dcn_docente dcn 
             inner join gen_usuario usr on usr.id=dcn.id_gen_usuario 
             left join cat_car_cargo_eisi car on car.id_cat_car=dcn.id_cargo_actual
