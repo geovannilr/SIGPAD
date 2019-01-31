076d57ead8d5b2447e759c49304b8115a4454c3a
Fernando Cosme <cosme.92@gmail.com>
2019-01-30 18:51:35 -0600

    autorizacionGrupos , formato word, falta parametrizar y mostrar la data.

--- app/Http/Controllers/TrabajoGraduacion/ReportesWordController.php
+++ app/Http/Controllers/TrabajoGraduacion/ReportesWordController.php
@@ -127,12 +127,93 @@ class ReportesWordController extends Controller{
 
         $objWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord, 'Word2007');
         try {
-            $objWriter->save(storage_path('helloWorld.docx'));
+            $objWriter->save(storage_path('frmt-acta-aprobacion-1.docx'));
         } catch (Exception $e) {
         }
 
 
-        return response()->download(storage_path('helloWorld.docx'));
+        return response()->download(storage_path('frmt-acta-aprobacion-1.docx'));
     }
 
+     public function autorizacionGrupos(Request $request){
+        $phpWord = new \PhpOffice\PhpWord\PhpWord();
+      $idGrupo = 16;
+      $grupo= new pdg_gru_grupoModel();
+      $server = $_ENV['SERVER'];
+        $estudiantesGrupo = $grupo->getDetalleGrupo($idGrupo);
+        $section = $phpWord->addSection();
+        if ($server  == 'ln') {
+          $imgPath = 'img/header.jpg';
+        }else{
+          $imgPath = 'img\header.jpg';
+        }
+        $meses = array("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
+        $dia = date('d');
+        $mes = date('n');
+        $nuevoMes =  $meses[$mes-1];
+        $anio = date('Y');
+       
+        $section->addText("Ref: EISI-000-".$anio,array('name' => 'New York', 'size' => 10),array('align'=>'right'));
+        $section->addText(" ");
+        $section->addText(" ");
+
+        $section->addText("Ciudad Universitaria,".$dia." de ".$nuevoMes." de ".$anio.".",array('name' => 'New York', 'size' => 10),array('align'=>'left'));
+        $section->addText(" ");
+      
+
+        $section->addText("Señores",array('name' => 'New York', 'size' => 10),array('align'=>'left'));
+        $section->addText("MIEMBROS DE LA JUNTA DIRECTIVA",array('name' => 'New York', 'size' => 10,'bold' => true),array('align'=>'left'));
+        $section->addText("FACULRAD DE INGENIERIA Y ARQUITECTURA",array('name' => 'New York', 'size' => 10),array('align'=>'left'));
+        $section->addText("Presente",array('name' => 'New York', 'size' => 10),array('align'=>'left'));
+        $section->addText(" ");
+        $section->addText("Respetables Señores",array('name' => 'New York', 'size' => 10),array('align'=>'left'));
+        $section->addText(" ");
+    
+        $section->addText("Reciban un saludo afectuoso, deseándoles  éxitos en el desempeño de sus labores. ",array('name' => 'New York', 'size' => 10),array('align'=>'left'));
+        
+       
+      $section->addText(" "); 
+
+      $cuerpo = "Para dar cumplimiento al artículo 193 de Reglamento de la Gestión Académico – Administrativa de la UES, solicito la autorización de los siguientes grupos de cuatro y cinco estudiantes para Trabajos de Graduación de la Escuela de Ingeniería de Sistemas Informáticos, y que han sido inscritos en el Ciclo I-2019. Estos grupos han sido conformados de esa forma, debido a la complejidad de los proyectos que van a desarrollar. Los grupos de estudiantes se detallan en la lista anexa. ";
+      $section->addText($cuerpo,array('name' => 'New York', 'size' => 10),array('align'=>'left'));
+      $section->addText(" ");
+      $section->addText(" ");
+      $section->addText("Agradeciendo su atención prestada, me suscribo de ustedes. ",array('name' => 'New York', 'size' => 10),array('align'=>'left'));
+       $section->addText(" ");
+       $section->addText(" ");
+
+       $section->addText('"HACIA LA LIBERTAD POR LA CULTURA"',array('name' => 'New York', 'size' => 10, 'bold' => true),array('align'=>'center'));
+    
+    
+   
+      $section->addText(" ");
+      $section->addText(" ");
+      $section->addText(" ");
+     
+      $section->addText('Ing. José María Sánchez Cornejo',array('name' => 'New York', 'size' => 10, 'bold' => false),array('align'=>'center'));
+      $section->addText('Director Escuela de Ingeniería de Sistemas Informáticos',array('name' => 'New York', 'size' => 10, 'bold' => false),array('align'=>'center'));
+
+      $section->addText(" ");
+      $section->addText(" ");
+
+      $section->addText("Anexo. Lista de grupos de cuatro y cinco estudiantes para Trabajos de Graduación. ",array('name' => 'New York', 'size' => 10),array('align'=>'left'));
+       $section->addText(" ");
+       $section->addText(" ");
+
+      $section->addText("Universidad de El Salvador",array('name' => 'New York', 'size' => 10),array('align'=>'left'));
+      $section->addText("Facultad de Ingeniería y Arquitectura",array('name' => 'New York', 'size' => 10),array('align'=>'left'));
+      $section->addText("Escuela de Ingeniería de Sistemas Informáticos",array('name' => 'New York', 'size' => 10,'bold'=>'true'),array('align'=>'left'));
+       $section->addText("Trabajos de Graduación",array('name' => 'New York', 'size' => 10,'bold'=>'true'),array('align'=>'left'));
+       $section->addText("Ciclo I 2019",array('name' => 'New York', 'size' => 10),array('align'=>'left'));
+       $section->addText(" ");
+       $section->addText('Lista de grupos de cuatro y cinco estudiantes',array('name' => 'New York', 'size' => 10, 'bold' => false),array('align'=>'center'));
+        $objWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord, 'Word2007');
+        try {
+            $objWriter->save(storage_path('frmt-autoriza-grupos-cuatro-cinco.docx'));
+        } catch (Exception $e) {
+        }
+
+
+        return response()->download(storage_path('frmt-autoriza-grupos-cuatro-cinco.docx'));
+    }
 }
